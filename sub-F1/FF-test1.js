625b3d2ba931b8f8369390cade5dc6bb7b0b52e5
Indunil1992 <indunil@adroitlogic.com>
2020-08-31 17:33:23 +0530

    1

---
+++ sub-F1/FF-test1.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = async (event) => {
+    try {
+        let data = await sns.publish({
+            Message: `jkjkjkjk`,
+            TopicArn: "arn:aws:sns:us-east-1:318300609668:Notifications",
+            MessageStructure: "String",
+            MessageAttributes: {}
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
+};
