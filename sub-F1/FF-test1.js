cb6844b8447d4bbf3005ff33d6e88f85291f7ffa
Indunil1992 <52061174+Indunil1992@users.noreply.github.com>
2020-08-31 18:02:24 +0530

    Update FF-test1.js

--- sub-F1/FF-test1.js
+++ sub-F1/FF-test1.js
@@ -3,16 +3,17 @@ const sns = new AWS.SNS();
 
 exports.handler = async (event) => {
     try {
+        //cmnt
         let data = await sns.publish({
-            Message: `jkjkjkjk`,
+            Message: `commit from git`,
             TopicArn: "arn:aws:sns:us-east-1:318300609668:Notifications",
             MessageStructure: "String",
             MessageAttributes: {}
         }).promise();
-
+//indu2 
     } catch (err) {
         // error handling goes here
     };
-
+//indu77
     return { "message": "Successfully executed-2" };
 };
